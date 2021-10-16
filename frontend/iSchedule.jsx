e916057587123d574ec2cd970f87921c3d86d38f
Yun Sung Choi <cyss0317@gmail.com>
2021-10-16 15:45:55 -0500

    Set up React

---
+++ frontend/iSchedule.jsx
@@ -0,0 +1,7 @@
+import React from "react/lib/React";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("root");
+    ReactDOM.render(<h1>welcome to iSchedule</h1>, root)
+})
